d8e5af67126f1bc45dbf4d0b56097ee996c54f4d
Darksoul01q <arun777q@gmail.com>
2026-08-27 23:16:22 +0530

    started day 22 challenges

---
+++ day22.js
@@ -0,0 +1,23 @@
+// mini challenges
+// challenge 1
+
+/* 
+the output order will be A>C>B>D
+*/
+
+// challenge 2
+
+/* 
+first the x in the fuction gets print than the x in the GEC will get print
+*/
+
+// challenge 3
+
+/* 
+the function call starts from the function call named third and stops it and starts second , then second stops
+and first will be executed , then 2 and lasty three
+*/
+
+// challenge 4
+
+/* */
