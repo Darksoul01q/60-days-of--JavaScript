533a6c86a72b264b14c327ff4489d7f0222aea90
Darksoul01q <arun777q@gmail.com>
2026-08-28 20:50:31 +0530

    started challenges on day 23

---
+++ day23.js
@@ -0,0 +1,25 @@
+//mini challenges
+
+// challenge 1
+
+/*  
+console.log(a);
+
+var a = 10;
+*/
+
+// js reads the console.log(a) first , the var declared after that , the output will be undefined
+
+// challenge 2
+
+/* console.log(a);
+
+let a = 10;
+
+ the js reads the console log first , it knows the let exist but cant access, cuz the let is in the temporal dead zone while js is executing
+ the print statement, output will be a reference error, cuz used let not var
+*/
+
+// challenge 3
+
+/*  */
