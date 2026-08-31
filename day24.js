601ed87f5f635c75d58a705a4ffd8089f0205938
Darksoul01q <arun777q@gmail.com>
2026-08-31 20:25:54 +0530

    completed all the challenges for day 24

--- day24.js
+++ day24.js
@@ -31,3 +31,103 @@ fn();
 output : undefined , cuz we didnt call the user.greet fucntion , we call fn()
 
 */
+
+// challenge 3
+
+/* 
+console.log(this === window);
+
+output : true , here this refers to window object 
+*/
+
+// challenge 4
+
+/* 
+function Student(name) {
+    this.name = name;
+}
+
+const student = new Student("Arun");
+
+console.log(student.name);
+
+exlpanation : see the const student , u can see that we using new keyword it creates a empty object passes into the
+Student constructor where the this.name will push the name property and return it to the student , now the const student become a student object holding 
+a name property, so if we print student.name it will print the value of the name key in the student object 
+*/
+
+//Challenge 5
+
+/* 
+function Student(name) {
+    this.name = name;
+}
+
+Student.prototype.greet = function () {
+    console.log("Hello " + this.name);
+};
+
+const student = new Student("Arun");
+
+student.greet();
+
+Explanation : after memory creation phase , when executing code the js engine see the student variable create a empty object , send inside 
+student constructor and return as a student object holding the name property, now if we call student.greet, the engine will the inside the object and not 
+find any greet named function , then it will see the prototype linked to its parent constructor and sees the greet function and access there , since 
+wee called using . notion student the this inside there will inherit the student object 
+*/
+
+//Challenge 6
+
+/* 
+const user = {
+    name: "Arun",
+
+    greet: () => {
+        console.log(this.name);
+    }
+};
+
+user.greet();
+
+yeah this is a regular function, if it is a constructor the function name will be begin with capital letter, and also used the new key word in the program
+the engine sees the user.greet , invoked the user object to find the greet function, it will find it and 
+the greet function will execute sees the this and it will inherit the user object and user.name and print Arun
+*/
+
+//Challenge 7
+
+/* 
+function Person(name) {
+    this.name = name;
+}
+
+Person.prototype.sayHello = function () {
+    console.log(`Hello ${this.name}`);
+};
+
+const person1 = new Person("Arun");
+const person2 = new Person("Rahul");
+
+console.log(person1.sayHello === person2.sayHello);
+
+
+output : it will print true , since we are checking person1 sayhello with person2 say hello ,  both the objects are linked with the same operator
+*/
+
+//Assignments
+
+// Assignment 1
+
+const student = {
+  name: "Arun",
+  age: 21,
+  course: "Full Stack",
+  introduce() {
+    console.log(`My name is : ${this.name}`);
+    console.log(`I am ${this.age} years old.`);
+    console.log(`I am learning ${this.course}`);
+  },
+};
+
+student.introduce();
