8cd9a7a30026c519f8bc707a62f71b1f4a4707d8
Darksoul01q <arun777q@gmail.com>
2026-09-01 23:01:10 +0530

    completed till assignment 4

--- day26.js
+++ day26.js
@@ -118,3 +118,98 @@ class Student extends Person {
 it will not work cuz , u see the super it is below the childs own constructor so it will throw an 
 error for sure 
 */
+
+//Assignments
+
+// Assignment 1
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  eat() {
+    console.log(`${this.name} will eat pet food`);
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log(`barks`);
+  }
+}
+
+let dog = new Dog("dog");
+
+dog.eat();
+dog.bark();
+
+// Assignment 2
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  constructor(name, age, course) {
+    super(name, age);
+    this.course = course;
+  }
+}
+
+let student1 = new Student("vishal", 21, "MERN");
+console.log(
+  `the name is ${student1.name} , i am a ${student1.age} years old ${student1.course} student`,
+);
+
+//Assignment 3
+class Vehicle {
+  constructor(brand) {
+    this.brand = brand;
+  }
+
+  start() {
+    console.log(`Vehicle Started`);
+  }
+}
+
+class Car extends Vehicle {
+  constructor(brand, model) {
+    super(brand);
+    this.model = model;
+  }
+
+  drive() {
+    super.start();
+    console.log(`car is driving`);
+  }
+}
+
+let car1 = new Car("toyota", "camry");
+console.log(car1.brand);
+console.log(car1.model);
+car1.drive();
+
+//Assignment 4
+
+class Color {
+  fav() {
+    console.log(`i like red color`);
+  }
+}
+
+class secondColor extends Color {
+  fav() {
+    console.log(`i like blue color`);
+  }
+}
+
+let person = new secondColor();
+person.fav();
+
+// used own example
+
+//assignment 5
