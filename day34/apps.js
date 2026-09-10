f17b11496e3b61b2919f7236aad8afa6c0ce423a
Darksoul01q <arun777q@gmail.com>
2026-09-10 19:58:57 +0530

    Assignment 1 done

---
+++ day34/apps.js
@@ -0,0 +1,7 @@
+//apps.js
+
+import { square, cube, isEven } from "./utils.js";
+
+console.log(square(5));
+console.log(cube(2));
+console.log(isEven(10));
---
+++ day34/utils.js
@@ -0,0 +1,13 @@
+//Assignment 1
+//utills.js
+export function square(num) {
+  return num ** 2;
+}
+
+export function cube(num) {
+  return num ** 3;
+}
+
+export function isEven(num) {
+  return num % 2 === 0;
+}
