a72098eef2c1a21662d2b0b8cd2978ed8d79183a
Darksoul01q <arun777q@gmail.com>
2026-09-14 09:43:37 +0530

    day 36 completed - done all challenges and assignments

--- day36.js
+++ day36.js
@@ -55,10 +55,71 @@ console.log(psCheck.test("Code 2026"));
 
 //Assignment 1
 console.log("Assignment 1");
-const emailCheck = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const emailCheck = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]+$/;
 
 console.log(emailCheck.test("user@example.com"));
 console.log(emailCheck.test("user.name@mail.org"));
 console.log(emailCheck.test("user@com"));
 console.log(emailCheck.test("user@domain.c"));
 console.log(emailCheck.test("user @domain.com"));
+
+//Assignment 2
+console.log("assignment 2");
+
+const numberCheck = /^[6789][0-9]{9}$/;
+
+console.log(numberCheck.test("9876543210"));
+console.log(numberCheck.test("7012345678"));
+console.log(numberCheck.test("5555555555"));
+console.log(numberCheck.test("987654321"));
+console.log(numberCheck.test("98765432101"));
+
+//Assignment 3
+
+console.log("Assignment 3");
+
+const text1 = "JavaScript is powerful. JavaScript is popular.";
+let matchRegex = /JavaScript/g;
+console.log(text1.match(matchRegex).length);
+console.log(text1.search(matchRegex));
+
+//Assignment 4
+console.log("Assignment 4");
+const message = "This message contains badword and anotherbadword.";
+
+console.log(message.replace(/badword/g, "******"));
+
+//Assignment 5
+
+console.log("Assignment 5");
+
+const usernameCheck = /^[a-zA-Z0-9]{6,12}$/;
+const EmailCheck = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]+$/;
+const passwordCheck = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d]{8,}$/;
+const mobileCheck = /^[6789][0-9]{9}$/;
+
+let userName = "Dark143q";
+let email = "arun143q@gmail.com";
+let ps = "Dark7777";
+let mobile = "6789456123";
+
+if (usernameCheck.test(userName)) {
+  console.log(`userName : valid`);
+} else {
+  console.log(`userName : Invalid`);
+}
+if (EmailCheck.test(email)) {
+  console.log(`email : valid`);
+} else {
+  console.log(`email : Invalid`);
+}
+if (passwordCheck.test(ps)) {
+  console.log(`password : valid`);
+} else {
+  console.log(`password : Invalid`);
+}
+if (mobileCheck.test(mobile)) {
+  console.log(`mobile : valid`);
+} else {
+  console.log(`mobile : Invalid`);
+}
